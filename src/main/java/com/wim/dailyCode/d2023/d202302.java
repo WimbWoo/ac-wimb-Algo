54c54ec1ab83f5ebe4415f0fed8e37acf2c57832
wimb <wimbwoo@163.com>
2023-02-08 23:19:32 +0800

    20230208 daily code

--- src/main/java/com/wim/dailyCode/d2023/d202302.java
+++ src/main/java/com/wim/dailyCode/d2023/d202302.java
@@ -1,7 +1,6 @@
 package com.wim.dailyCode.d2023;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class d202302 {
     public static void main(String[] args) {
@@ -25,5 +24,55 @@ public class d202302 {
             sb.append(map.get(message.charAt(i)));
         }
         return sb.toString();
+    }
+
+    /**
+     * 1604. 警告一小时内使用相同员工卡大于等于三次的人
+     */
+    public List<String> alertNames(String[] keyName, String[] keyTime) {
+        Map<String, List<Integer>> nameMapToTime = new HashMap<>();
+        // 1. put into map <Name List<Time>>
+        for (int i = 0; i < keyName.length; i++) {
+            String hour = keyTime[i].substring(0, 2);
+            String min = keyTime[i].substring(3);
+            int time = Integer.parseInt(hour) * 60 + Integer.parseInt(min);
+            nameMapToTime.computeIfAbsent(keyName[i], t -> new ArrayList<>()).add(time);
+        }
+        // 2.compare time to find answer
+        List<String> res = new ArrayList<>();
+        for (String name : nameMapToTime.keySet()) {
+            List<Integer> timesList = nameMapToTime.get(name);
+            int size = timesList.size();
+            Collections.sort(timesList);
+            if (size > 2) {
+                for (int i = 2; i < size; i++) {
+                    if (timesList.get(i) - timesList.get(i - 2) <= 60) {
+                        res.add(name);
+                        break; // remove duplicate name
+                    }
+                }
+            }
+        }
+        Collections.sort(res);
+        return res;
+    }
+
+    /**
+     * 1233. 删除子文件夹
+     * method1: sort
+     * method2: 字典树
+     */
+    public List<String> removeSubfolders(String[] folder) {
+        Arrays.sort(folder);
+        List<String> ans = new ArrayList<>();
+        ans.add(folder[0]);
+        for (int i = 1; i < folder.length; ++i) {
+            int m = ans.get(ans.size() - 1).length();
+            int n = folder[i].length();
+            if (m >= n || !(ans.get(ans.size()-1).equals(folder[i].substring(0, m)) && folder[i].charAt(m) == '/')) {
+                ans.add(folder[i]);
+            }
+        }
+        return ans;
     }
 }
